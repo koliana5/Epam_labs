04d45fe45eab8799f40f283c273fc9021ee48467
Mikalai Ulasevich <koliana5399553@gmail.com>
2019-10-08 00:30:35 +0300

    unit

---
+++ Unit_test/UnitTest/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangle;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsTriangleAllZeroTest()
+        {
+            Assert.IsFalse(WorkWithTriangles.IsTriangle(0, 0, 0));
+        }
+        [TestMethod]
+        public void IsTriangleThreeIdenticalTest()
+        {
+            Assert.IsTrue(WorkWithTriangles.IsTriangle(5, 5, 5));
+        }
+
+        [TestMethod]
+        public void IsTriangleTwoIdenticalTest()
+        {
+            Assert.IsTrue(WorkWithTriangles.IsTriangle(4, 4, 3));
+        }
+
+        [TestMethod]
+        public void IsTriangleRightTest()
+        {
+            Assert.IsTrue(WorkWithTriangles.IsTriangle(5, 8, 10));
+        }
+
+        [TestMethod]
+        public void IsTriangleBadTest()
+        {
+            Assert.IsFalse(WorkWithTriangles.IsTriangle(3, 4, 9));
+        }
+
+        [TestMethod]
+        public void IsTriangleGoodTest()
+        {
+            Assert.IsTrue(WorkWithTriangles.IsTriangle(3, 4, 5));
+        }
+
+        [TestMethod]
+        public void IsTriangleOneNegativeSideTest()
+        {
+            Assert.IsFalse(WorkWithTriangles.IsTriangle(3, -4, 5));
+        }
+
+        [TestMethod]
+        public void IsTriangleTwoNegativeSidesGoodTest()
+        {
+            Assert.IsFalse(WorkWithTriangles.IsTriangle(-3, -4, 5));
+        }
+
+        [TestMethod]
+        public void IsTriangleThreeNegativeSidesGoodTest()
+        {
+            Assert.IsFalse(WorkWithTriangles.IsTriangle(-3, -4, -5));
+        }
+
+        [TestMethod]
+        public void IsTriangleDoubleGoodTest()
+        {
+            Assert.IsTrue(WorkWithTriangles.IsTriangle(3.9, 1.9, 5.7));
+        }
+    }
+}
---
+++ Unit_test/Unit_tests/WorkWithTriangles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public class WorkWithTriangles
+    {
+        public static bool IsTriangle(double a,double b,double c) 
+        {
+            return ((a > 0) && (b > 0) && (c > 0) && (a + b > c) && (c + a > b) && (b + c > a));
+        }
+    }
+}
